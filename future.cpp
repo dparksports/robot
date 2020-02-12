6048759263457ba7f58d612f16b7dd1d6422a60b
Dan Park <dpark.sports@gmail.com>
2020-02-12 15:55:21 -0800

    added lock_guard

--- future.cpp
+++ future.cpp
@@ -1,13 +1,15 @@
 #include <vector>
-#include <thread>
-#include <future>
 #include <numeric>
 #include <iostream>
 #include <chrono>
 
+#include <thread>
+#include <future>
+#include <mutex>
+
 using namespace std;
 
-int detach() {
+void detach() {
     promise<int> taskPromise;
     std::future<int> taskFuture = taskPromise.get_future();
     std::thread robotThread = std::thread([&taskPromise]{
@@ -22,7 +24,6 @@ int detach() {
 
 //    robotThread.join();
     cout << "ending" << '\n';
-    return 0;
 }
 
 void workTask1() {
@@ -31,14 +32,13 @@ void workTask1() {
     std::cout << "ended a robot task." << std::endl;
 }
 
-int detachJoin() {
+void detachJoin() {
     thread threadRobot(workTask1);
     threadRobot.detach();
 //    threadRobot.join();
 
     this_thread::sleep_for(chrono::seconds(2));
     std::cout << "exiting main.." << std::endl;
-    return 0;
 }
 
 
@@ -56,8 +56,7 @@ void do_work(std::promise<void> barrier)
     barrier.set_value();
 }
 
-int signalThreadsUsingPromise()
-{
+void signalThreadsUsingPromise() {
 //    // Demonstrate using promise<int> to transmit a result between threads.
 //    std::vector<int> numbers = { 1, 2, 3, 4, 5, 6 };
 //    std::promise<int> accumulate_promise;
@@ -86,7 +85,7 @@ void workTask2(std::promise<void> taskPromise) {
     std::cout << "ended a robot task." << std::endl;
 }
 
-int main() {
+void setValueByPromise() {
     promise<void> taskPromise;
     future<void> taskFuture = taskPromise.get_future();
     thread robotThread(workTask2, move(taskPromise));
@@ -95,4 +94,74 @@ int main() {
     cout << "starting join" << endl;
     robotThread.join();
     cout << "ending join" << endl;
+
+}
+
+int g_i = 0;
+std::mutex g_i_mutex;  // protects g_i
+
+void safe_increment()
+{
+    const std::lock_guard<std::mutex> lock(g_i_mutex);
+    ++g_i;
+
+    std::cout << std::this_thread::get_id() << ": " << g_i << '\n';
+
+    // g_i_mutex is automatically released when lock
+    // goes out of scope
+}
+
+void lockGuardMutex()
+{
+    std::cout << "main: " << g_i << '\n';
+
+    std::thread t1(safe_increment);
+    std::thread t2(safe_increment);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "main: " << g_i << '\n';
+}
+
+mutex whiteboard1;
+int occupyNode(int nodeIdj, int nodeValue) {
+    const lock_guard<std::mutex> taskLockGuard(whiteboard1);
+
+    nodeValue++;
+    cout << this_thread::get_id() << " nodeIdj: " << nodeIdj << " nodeValue: " << nodeValue << '\n';
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    return nodeValue;
+}
+
+int main() {
+    int nodeValue = 1;
+    int nodeId = 123;
+
+    std::cout << "nodeId: " << nodeId << '\n';
+    std::cout << "nodeValue: " << nodeValue << '\n';
+
+    std::packaged_task<int(int, int)> packagedTask(occupyNode);
+    std::future<int> resultFuture = packagedTask.get_future();
+    std::thread t1(std::move(packagedTask), nodeId, nodeValue);
+
+    std::packaged_task<int(int, int)> packaged2(occupyNode);
+    std::future<int> resultFuture2 = packaged2.get_future();
+    std::thread t2(std::move(packaged2), nodeId, nodeValue);
+
+    t1.detach();
+    t2.detach();
+
+    nodeValue = resultFuture.get();
+    std::cout << "nodeValue: " << nodeValue << '\n';
+//    t1.join();
+
+
+//    t2.join();
+    nodeValue = resultFuture2.get();
+    std::cout << "nodeValue: " << nodeValue << '\n';
+
+    std::cout << "nodeId: " << nodeId << '\n';
+    std::cout << "nodeValue: " << nodeValue << '\n';
 }
